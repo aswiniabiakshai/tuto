2128876813e73c763411528e6fbfc60192cd49a9
aswinisathasivan <aswiniabiakshai>
2025-02-06 13:39:15 +0530

    bii1

--- ash.c
+++ ash.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 int main(){
-    unsigned char a=255;
-    printf("%d",a);
+    int num;
+    printf("enter a num:");
+    scanf("%d",&num);
+    if(num%3==0 & num%5==0){
+    printf("the num is divisible by both 3 and 5",num);
+    }
+    else{
+    printf("the num is not divisible by both 3 and 5",num);
+    }
     return 0;
 }
